3081d183d19bc0342f9c0d87110ab529811b6d37
LeonardoSoare <leonardomateisoare@gmail.com>
2022-11-03 14:13:42 +0200

    mobile completed

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ const sectionFeature = document.querySelector(".features-section");
 const aboutSection = document.querySelector(".about-section");
 const navHeight = navigation.getBoundingClientRect().height;
 const companyLogo = document.querySelector(".company-logo").firstElementChild;
+const bodyLabel = document.querySelector("body");
+const menu = document.querySelector(".menu");
+const openMenu = document.querySelector(".burger-menu");
+const closeMenu = document.querySelector(".close");
+const navText = document.querySelector(".nav-texts");
 // console.log(companyLogo);
 console.log(navHeight);
 
-//Sticky navigation
+//Universal mechanism
+
+// Sticky navigation
 const stickyNav = function (entries) {
   const [entry] = entries;
   console.log(entry);
@@ -39,14 +46,36 @@ btnLearnMore.addEventListener("click", function (e) {
 });
 navigation.addEventListener("click", function (e) {
   e.preventDefault();
+  if (e.target === openMenu || e.target === closeMenu) return;
   const id = e.target.getAttribute("href");
   console.log(id);
   document.querySelector(`${id}`).scrollIntoView({ behavior: "smooth" });
 });
 
-// const bodylabel = document.querySelector("body");
-// const mq = window.matchMedia("(max-width: 50.25em)");
-// console.log(mq.matches);
+//for media Queries
+
+const mq = window.matchMedia("(max-width: 50.25em)");
+console.log(mq.matches);
+if (mq.matches) {
+  openMenu.classList.add("active");
+  menu.addEventListener("click", function (e) {
+    openMenu.classList.toggle("active");
+    closeMenu.classList.toggle("active");
+    if (closeMenu.classList.contains("active")) {
+      navText.style.left = 0;
+    } else navText.style.left = `100%`;
+  });
+}
+// function isOverflown(element) {
+//   if (element.scrollHeight > element.clientHeight) return element;
+//   else return "";
+// }
+// const arr = [...document.getElementsByTagName("div")];
+// arr.forEach(function (el) {
+//   console.log(isOverflown(el));
+// });
+// console.log();
+// console.log(isOverflown(document.querySelector("main")));
 
 // const mqTestFunction = function () {
 //   if (mq.matches) {
